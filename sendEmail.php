850862bc55f8519f0a0dc153969b6709056076a6
Hanmi Moon <hmmoon@hotmail.com>
2021-02-18 23:40:15 -0700

    3yhsdhf

--- sendEmail.php
+++ sendEmail.php
@@ -13,7 +13,7 @@
         require_once = "PHPMailer/SMTP.php";
         require_once = "PHPMailer/Exception.php";
 
-        $mail new PHPMailer(); 
+        $mail = new PHPMailer(); 
 
         $mail->isSMTP();
         $mail->Host = "smtp.gmail.com";
