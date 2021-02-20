77cdf0927d97c9320592b36c5b4a343b7638e616
Hanmi Moon <hmmoon@hotmail.com>
2021-02-19 21:51:17 -0700

    downloaded composer and trying to send email

--- sendEmail.php
+++ sendEmail.php
@@ -1,45 +1,48 @@
 
 
-    <?php 
-    use PHPMailer\PHPMailer\PHPMailer;
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-    if(isset($_POST['name']) && isset($_POST['email'])){
-        $name = $_POST['name'];
-        $subject = $_POST['subject'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-        require_once = "PHPMailer/PHPMailer.php";
-        require_once = "PHPMailer/SMTP.php";
-        require_once = "PHPMailer/Exception.php";
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-        $mail = new PHPMailer(true); 
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'doxie-ology@gmail.com.com';                     //SMTP username
+    $mail->Password   = 'Cosprings123';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-        $mail->isSMTP();
-        $mail->Host = "smtp.gmail.com";
-        $mail->SMTPAuth = true;
-        $mail->Username = "doxieology.mcafee@gmail.com";
-        $mail->Password = "Cosprings123";
-        $mail->Port = 587;
-        $mail->SMTPSecure = "tls";
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress("doxieology.mcafee@gmail.com");
+    $mail->Subject = ("$email ($subject)");
+    $mail->Message = $message;              //Name is optional
 
-        $mail->isHTML(true);
-        $mail->setFrom($email, $name);
-        $mail->addAddress("doxieology.mcafee@gmail.com");
-        $mail->Subject = ("$email ($subject)");
-        $mail->Message = $message;
 
-        if($mail->send()){ 
-            $response = "Email is sent!";
 
-        }
-        else 
-        {
-            $status = "failed";
-            $response = "Something went wrong: <br>" . $mail->ErrorInfo; 
-        }
-        exit(json_encode(arrary("status" => $status, "response" => $response)));
-    
-    }
 
-    ?>
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
