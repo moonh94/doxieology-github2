9c5fccac952493a94d215efe775dc1516cde97c8
Hanmi Moon <hmmoon@hotmail.com>
2021-03-03 19:12:28 -0700

    update profile pics of beau and duncan

--- sendEmail.php
+++ sendEmail.php
@@ -17,7 +17,7 @@ if(isset($_POST['name']) && isset($_POST['email'])){
   $mail->Host = "smtp.gmail.com";
   $mail->SMTPAuth = true;
   $mail->Username = "doxieology2019@gmail.com";
-  $mail->Password = "Donte2019";
+  $mail->Password = "Dave@Denise2019";
   $mail->Port = 465;
   $mail->SMTPSecure = "ssl";
 
