8d15c80cfa1fc54c938cd8addb72801a818a1933
Hanmi Moon <hmmoon@hotmail.com>
2021-04-10 21:55:07 -0600

    email was listed wrong

--- sendEmail.php
+++ sendEmail.php
@@ -15,8 +15,7 @@ if(isset($_POST['name']) && isset($_POST['email'])){
   $mail->isSMTP();
   $mail->Host = "smtp.gmail.com";
   $mail->SMTPAuth = true;
-  $mail->Username = "doxieology.mcafee@gmail.com
-  ";
+  $mail->Username = "doxieology.mcafee@gmail.com";
   $mail->Password = "StudiO420";
   $mail->Port = 465;
   $mail->SMTPSecure = "ssl";
